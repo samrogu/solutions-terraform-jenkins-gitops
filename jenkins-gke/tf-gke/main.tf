059863db708f6a98b0725ae88208857ff814af42
Samuel Guardado <grd.rodrigez13@gmail.com>
2021-06-28 05:18:52 +0000

    Change versions chart helm, repo , jenkins and  update plugins

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -188,9 +188,9 @@ data "local_file" "helm_chart_values" {
 
 resource "helm_release" "jenkins" {
   name       = "jenkins"
-  repository = "https://kubernetes-charts.storage.googleapis.com"
+  repository = "https://charts.jenkins.io"
   chart      = "jenkins"
-  version    = "1.9.18"
+  version    = "2.5.4"
   timeout    = 1200
 
   values = [data.local_file.helm_chart_values.content]
